da74c6e292f2fc82790193e8e0c64ae9dd6aa24a
mujeeb <khanmujeeb687@gmail.com>
2022-10-12 16:59:20 +0200

    updating facebook sdk

--- packages/appinio_social_share/ios/Classes/ShareUtil.swift
+++ packages/appinio_social_share/ios/Classes/ShareUtil.swift
@@ -2,7 +2,6 @@ import Photos
 import FBSDKCoreKit
 import FBSDKShareKit
 import Social
-import TikTokOpenSDK
 
 public class ShareUtil{
     
@@ -84,12 +83,12 @@ public class ShareUtil{
         let message = args[self.argMessage] as? String
         let imagePath = args[self.argImagePath] as? String
         
-        let photo = SharePhoto(image: UIImage.init(contentsOfFile: imagePath!)!, userGenerated: true)
+        let photo = SharePhoto(image: UIImage.init(contentsOfFile: imagePath!)!, isUserGenerated: true)
         let content = SharePhotoContent()
         content.photos = [photo]
         content.hashtag = Hashtag(message!)
         let dialog = ShareDialog(
-            fromViewController: UIApplication.shared.windows.first!.rootViewController,
+            viewController: UIApplication.shared.windows.first!.rootViewController,
             content: content,
             delegate: delegate
         )
